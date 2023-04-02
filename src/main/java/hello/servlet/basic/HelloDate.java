73db488fbb9e16a4b53cf7d79f864452299ca8e7
fbehddn <tom0611@naver.com>
2023-04-02 17:44:10 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/hello/servlet/basic/HelloDate.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloDate {
+
+    private String username;
+    private int age;
+
+
+
+    // 롬복으로 생략 가능
+
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+}
